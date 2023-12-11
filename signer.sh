8d738272c03be4a6f2080dc1fb9da33287206e50
enriquebelarte <ebelarte@redhat.com>
2023-12-11 07:23:32 -0600

    Add dummy sign of .ko files

--- signer.sh
+++ signer.sh
@@ -5,8 +5,8 @@ openssl req -x509 -new -nodes -utf8 -sha256 -days 36500 -batch \
       -config x509-configuration.ini \
       -outform DER -out $PATH_KEY/public_key.der \
       -keyout $PATH_KEY/private/private_key.priv
-modules=("nvidia-drm","nvidia","nvidia-modeset","nvidia-peermem","nvidia-uvm")
-for MODULE in ${modules[@]}; do
+modules=(nvidia-drm nvidia nvidia-modeset nvidia-peermem nvidia-uvm)
+for MODULE in "${modules[@]}"; do
 /usr/src/kernels/${KSOURCES}/scripts/sign-file \
           sha256 \
 	  $PATH_KEY/private/private_key.priv \
